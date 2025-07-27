89c62f80ea8a48413c5b060c142288a3545a8593
Felipe Lopes <7880548+felops@users.noreply.github.com>
2025-07-27 00:18:30 -0300

    feat: increase establishment names and their ratings font size to 20px

--- src/components/EstablishmentsTable.tsx
+++ src/components/EstablishmentsTable.tsx
@@ -1,40 +1,30 @@
-import React from "react";
-import { EstablishmentsTableRow } from "./EstablishmentsTableRow";
-import PropTypes from "prop-types";
-
 const headerStyle: { [key: string]: string | number } = {
   paddingBottom: "10px",
   textAlign: "left",
   fontSize: "20px",
 };
 
-export const EstablishmentsTable: React.FC<{
+export const EstablishmentsTable = ({
+  establishments
+}: {
   establishments: { [key: string]: string }[] | null | undefined;
-}> = ({ establishments }) => {
+}) => {
   return (
     <table>
-      <tbody>
+      <thead>
         <tr>
           <th style={headerStyle}>Business Name</th>
           <th style={headerStyle}>Rating Value</th>
         </tr>
-        {establishments &&
-          establishments?.map(
-            (
-              establishment: { [key: string]: string } | null | undefined,
-              index: React.Key | null | undefined
-            ) => (
-              <EstablishmentsTableRow
-                key={index}
-                establishment={establishment}
-              />
-            )
-          )}
+      </thead>
+      <tbody>
+        {establishments && establishments?.map(establishment => (
+          <tr style={{ fontSize: "20px" }}>
+            <td>{establishment?.BusinessName}</td>
+            <td>{establishment?.RatingValue}</td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
-};
-
-EstablishmentsTable.propTypes = {
-  establishments: PropTypes.array,
 };
